3c161954330f0304abbeb9572a288d78b46b554c
Gabriel da Silva Seles <avanade.gseles@voeazul.com.br>
2023-04-18 21:24:41 -0300

    add music playlist

---
+++ Models/IncludeMusicPlaylist.cs
@@ -0,0 +1,8 @@
+﻿namespace CatolicoCantorWeb.Models
+{
+    public class IncludeMusicPlaylist
+    {
+        public int MusicId { get; set; }
+        public int PlaylistId { get; set; }
+    }
+}
--- Models/Music.cs
+++ Models/Music.cs
@@ -10,15 +10,16 @@ namespace CatolicoCantorWeb.Models
         public string Nome { get; set; }
         [Required(ErrorMessage ="Cantor é obrigatório")]
         public string Cantor { get; set; }
-        [Required(ErrorMessage ="Categoria é obrigatório")]
-        public List<string> Categorias { get; set; }
+        //[Required(ErrorMessage ="Categoria é obrigatório")]
         [Required(ErrorMessage ="Letra é obrigatório")]
         public string Letra { get; set; }
         [Required(ErrorMessage ="Cifra é obrigatório")]
         public string Cifra { get; set; }
         [Required(ErrorMessage ="Youtube é obrigatório")]
         public string Youtube { get; set; }
-        [Required(ErrorMessage ="Tags é obrigatório")]
+        //[Required(ErrorMessage ="Tags é obrigatório")]
         public string Tags { get; set; }
+        public List<Category> Categories { get; set; } = new List<Category> { };
+
     }
 }
