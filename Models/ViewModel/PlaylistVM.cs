88c497cd149cc08ee5bb2bb6df5f50f2d4658e23
Gabriel da Silva Seles <avanade.gseles@voeazul.com.br>
2023-04-23 16:51:39 -0300

    Ajustes gerais

---
+++ Models/ViewModel/PlaylistVM.cs
@@ -0,0 +1,13 @@
+﻿namespace CatolicoCantorWeb.Models.ViewModel
+{
+    public class PlaylistVM
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int IdUsuario { get; set; }
+        public bool Publica { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public List<Music> Musics { get; set; }
+    }
+}
